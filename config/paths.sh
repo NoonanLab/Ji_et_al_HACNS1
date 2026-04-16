b79ed80ac3ba6c811d639e6463e0d1a6b71f3e20
yuji8922 <100238875+yuji8922@users.noreply.github.com>
2026-04-16 15:25:14 -0400

    Create paths.sh

---
+++ config/paths.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PROJECT_ROOT="/vast/palmer/scratch/noonan/yj345"
+GENOME_SIZE="/gpfs/gibbs/pi/noonan/yj345/genome/mm10.chrom.sizes"
+HUMAN_REF="/gpfs/gibbs/project/noonan/yj345/makeHumanizedFasta/genome/hmm10"
+CHIMP_REF="/gpfs/gibbs/project/noonan/yj345/makeHumanizedFasta/genome/cmm10"
+CONTIGS_MOUSE="contigs_mouse.txt"
