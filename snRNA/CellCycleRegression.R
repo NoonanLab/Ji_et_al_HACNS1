e9560ef7ef17043a21b4419b2f25fd3586a330a6
yuji8922 <100238875+yuji8922@users.noreply.github.com>
2026-04-17 11:08:50 -0400

    Create CellCycleRegression.R

---
+++ snRNA/CellCycleRegression.R
@@ -0,0 +1,9 @@
+.libPaths(c("/gpfs/gibbs/project/noonan/yj345/R/4.2", .libPaths()))
+
+setwd("/vast/palmer/scratch/noonan/yj345/new")
+
+library(Seurat)
+library(dplyr)
+library(cowplot)
+library(stringr)
+
