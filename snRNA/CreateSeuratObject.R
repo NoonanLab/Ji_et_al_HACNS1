23ad95294744ab965e6e69beb12cea78c6bfa03c
yuji8922 <100238875+yuji8922@users.noreply.github.com>
2026-04-17 11:12:11 -0400

    Update CreateSeuratObject.R

--- snRNA/CreateSeuratObject.R
+++ snRNA/CreateSeuratObject.R
@@ -43,5 +43,35 @@ sample_paths <- sample_paths[grep("_Velocyto$", sample_paths)]
 
 # Process each sample
 for (sample_path in sample_paths) {
-  process_sample(sample_path, project_name = "PA", output_dir = output_dir)
+  process_sample(sample_path, project_name = "HACNS1", output_dir = output_dir)
 }
+
+CFLE9_Velocyto <- subset(CFLE9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HFLE9_Velocyto <- subset(HFLE9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CFLE10_Velocyto <- subset(CFLE10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HFLE10_Velocyto <- subset(HFLE10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CFLE11_Velocyto <- subset(CFLE11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HFLE11_Velocyto <- subset(HFLE11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CFLE12_Velocyto <- subset(CFLE12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HFLE12_Velocyto <- subset(HFLE12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CHLE10_Velocyto <- subset(CHLE10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HHLE10_Velocyto <- subset(HHLE10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CHLE11_Velocyto <- subset(CHLE11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HHLE11_Velocyto <- subset(HHLE11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CHLE12_Velocyto <- subset(CHLE12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HHLE12_Velocyto <- subset(HHLE12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CME12_Velocyto <- subset(CME12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HME12_Velocyto <- subset(HME12_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+CPA1E9_Velocyto <- subset(CPA1E9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HPA1E9_Velocyto <- subset(HPA1E9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+CPA1E10_Velocyto <- subset(CPA1E10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HPA1E10_Velocyto <- subset(HPA1E10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+CPA1E11_Velocyto <- subset(CPA1E11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HPA1E11_Velocyto <- subset(HPA1E11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CPA2E9_Velocyto <- subset(CPA2E9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HPA2E9_Velocyto <- subset(HPA2E9_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+CPA2E10_Velocyto <- subset(CPA2E10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+HPA2E10_Velocyto <- subset(HPA2E10_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+CPA2E11_Velocyto <- subset(CPA2E11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 1)
+HPA2E11_Velocyto <- subset(HPA2E11_Velocyto, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 1)
+
