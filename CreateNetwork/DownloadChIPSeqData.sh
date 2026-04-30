eb0cd07e7d3aa1962881e84952a4e07d194db071
yuji8922 <100238875+yuji8922@users.noreply.github.com>
2026-04-30 09:32:00 -0400

    Create DownloadChIPSeqData.sh

---
+++ CreateNetwork/DownloadChIPSeqData.sh
@@ -0,0 +1,22 @@
+module load miniconda
+conda activate sra
+
+#Md
+prefetch SRR4835044
+fasterq-dump SRR4835044 -e 8 -p -O .
+
+#PA2
+prefetch SRR4835045
+fasterq-dump SRR4835045 -e 8 -p -O .
+
+#FL
+prefetch SRR6294689
+fasterq-dump SRR6294689 -e 8 -p -O .
+prefetch SRR6294690
+fasterq-dump SRR6294690 -e 8 -p -O .
+
+#HL
+prefetch SRR3950341
+fasterq-dump SRR3950341 -e 8 -p -O .
+prefetch SRR3950342
+fasterq-dump SRR3950342 -e 8 -p -O .
